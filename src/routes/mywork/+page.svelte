ea800e66100bb0344851fb5d25db4ac2af00c1bd
Hadil24A <hadil.ahmed@hva.nl>
2024-12-07 22:30:42 +0100

    update my work

--- src/routes/mywork/+page.svelte
+++ src/routes/mywork/+page.svelte
@@ -9,86 +9,126 @@
   <h1>My work</h1>
   <ul class="my-work">
     <li class="work">
-      <a href="https://github.com/Hadil66/fabrique" target="_blank"><h2>Fabrique: Qatar Museums</h2></a>
-      <img src="/images/fabrique.png" alt="fabrique"> 
+        <div class="container1">
+        <h2>Fabrique: Qatar Museums</h2>
+        <img src="/images/fabrique.png" alt="fabrique" />
+    </div>
+    <div class="container2">
+        <p></p>
+        <a href="https://github.com/Hadil66/fabrique" target="_blank">GitHub</a>
       <ul>
         <li>Sveltekit</li>
         <li>Masonry</li>
         <li>Accessibility</li>
         <li>Directus</li>
       </ul>
+    </div>
     </li>
     <li class="work">
-      <a href="https://github.com/Hadil66/your-tribe-for-life-squad-page" target="_blank"><h2>Squadpage 2</h2></a>
-      <img src="/images/squadpage2.png" alt="squadpage 2">
+        <div class="container1">
+        <h2>Squadpage 2</h2>
+        <img src="/images/squadpage2.png" alt="squadpage 2" />
+    </div>
+    <div class="container2">
+        <p></p>
+        <a href="https://github.com/Hadil66/your-tribe-for-life-squad-page" target="_blank">GitHub</a>
       <ul>
         <li>Sveltekit</li>
         <li>Directus</li>
       </ul>
+    </div>
     </li>
     <li class="work">
-      <a href="https://github.com/Hadil66/your-tribe-for-life-profile-card" target="_blank"><h2>Profilecard 2</h2></a>
-      <img src="/images/profilecard2.png" alt="profilecard 2">
+        <div class="container1">
+        <h2>Profilecard 2</h2>
+        <img src="/images/profilecard2.png" alt="profilecard 2" />
+    </div>
+        <p></p>
+        <div class="container2">
+        <a href="https://github.com/Hadil66/your-tribe-for-life-profile-card" target="_blank">GitHub</a>
       <ul>
         <li>Sveltekit</li>
         <li>JavaScript Animations</li>
         <li>Directus</li>
       </ul>
+    </div>
     </li>
     <li class="work">
-      <a href="https://github.com/Hadil66/proof-of-concept" target="_blank"><h2>DPI: TNO</h2></a>
-      <img src="/images/dpi.png" alt="dpi">
+        <div class="container1">
+        <h2>DPI: TNO</h2>
+        <img src="/images/dpi.png" alt="dpi" />
+    </div>
+        <p></p>
+        <div class="container2">
+        <a href="https://github.com/Hadil66/proof-of-concept" target="_blank">GitHub</a>
       <ul>
         <li>EJS</li>
         <li>NodeJS</li>
         <li>Express</li>
         <li>Directus</li>
       </ul>
+    </div>
     </li>
     <li class="work">
-      <a href="https://github.com/Hadil66/Pleasurable-UI" target="_blank"><h2>Tumi Mundo</h2></a>
-      <img src="/images/tumimundo.png" alt="tumi mundo">
+        <div class="container1">
+        <h2>Tumi Mundo</h2>
+        <img src="/images/tumimundo.png" alt="tumi mundo" />
+    </div>
+        <p></p>
+        <div class="container2">
+        <a href="https://github.com/Hadil66/Pleasurable-UI" target="_blank">GitHub</a>
       <ul>
         <li>EJS</li>
         <li>NodeJS</li>
         <li>Express</li>
         <li>Directus</li>
       </ul>
+    </div>
     </li>
     <li class="work">
-      <a href="https://github.com/Hadil66/connect-your-tribe-profile-card" target="_blank"><h2>Profilecard 1</h2></a>
-      <img src="/images/profilecard1.png" alt="profilecard 1">
+        <div class="container1">
+        <h2>Profilecard 1</h2>
+        <img src="/images/profilecard1.png" alt="profilecard 1" />
+    </div>
+        <p></p>
+        <div class="container2">
+        <a href="https://github.com/Hadil66/connect-your-tribe-profile-card" target="_blank">GitHub</a>
       <ul>
         <li>EJS</li>
         <li>NodeJS</li>
         <li>Express</li>
       </ul>
+    </div>
     </li>
     <li class="work">
-      <a href="https://github.com/Hadil66/Label-A-teampagina" target="_blank"k><h2>Label A</h2></a>
-      <img src="/images/labela.png" alt="label a">
+        <div class="container1">
+        <h2>Label A</h2>
+        <img src="/images/labela.png" alt="label a" />
+    </div>
+        <p></p>
+        <div class="container2">
+        <a href="https://github.com/Hadil66/Label-A-teampagina" target="_blank">GitHub</a>
       <ul>
         <li>Accessibility</li>
         <li>Responsive</li>
         <li>Express</li>
       </ul>
+    </div>
     </li>
     <li class="work">
-      <a href="https://github.com/Hadil66/the-client-website" target="_blank"><h2>Funda</h2></a>
-      <img src="/images/funda.png" alt="funda">
+        <div class="container1">
+            <h2>Funda</h2>
+            <img src="/images/funda.png" alt="funda" />
+        </div>
+        <p></p>
+        <div class="container2">
+        <a href="https://github.com/Hadil66/the-client-website" target="_blank">GitHub</a>
       <ul>
         <li>Accessibility</li>
         <li>Responsive</li>
         <li>Micro interaction</li>
       </ul>
-    </li>
-    <li class="work">
-      <a href="https://github.com/Hadil66/your-tribe-squad-page" target="_blank"><h2>Squadpage 1</h2></a>
-      <img src="/images/squadpage1.png" alt="squadpage 1">
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-      </ul>
+    </div>
     </li>
   </ul>
 </main>
@@ -104,55 +144,55 @@
   }
 
   .my-work {
+    align-items: center;
     display: flex;
+    flex-direction: column;
     flex-wrap: nowrap;
     gap: 1em;
-    overflow-x: scroll;
-    margin: calc(var(--reg-space) /100);
-    padding: calc(var(--reg-space) /100);
-    width: 100vw;
-    z-index: 78;
+    padding: calc(var(--reg-space) / 100);
   }
 
-  .my-work::-webkit-scrollbar {
-    display: none;
-}
-
   .work {
-    align-items: center;
-    background: var(--see-through-dark);
     border-radius: var(--s-radius);
     box-shadow: var(--shadow);
     display: flex;
-    flex-direction: column;
-    height: 30em;
+    height: 18em;
     list-style-type: "";
-    gap: var(--reg-space);
     margin: var(--reg-space) 0 var(--reg-space) 0;
-    min-width: 20em;
     padding: var(--reg-space);
+    width: 80vw;
     z-index: 78;
   }
-
-  a {
-    text-decoration: none;
+  
+  .container1 {
+    display: flex;
+    flex-direction: column;
   }
 
   .work img {
-    width: 400px;
+    width: 350px;
+  }
+
+  .container2 {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+
+  .work p {
   }
 
   .work ul {
     padding: 0;
     display: flex;
     flex-wrap: wrap;
-}
-.work li {
-  list-style-type: "";
-  border: solid 1px;
-  margin: calc(var(--reg-space)/5);
-  padding: calc(var(--reg-space)/3);
-  width: max-content;
-  border-radius: 50px;
-}
+  }
+  .work li {
+    border: solid 1px;
+    border-radius: 50px;
+    list-style-type: "";
+    margin: calc(var(--reg-space) / 5);
+    padding: calc(var(--reg-space) / 3);
+    width: max-content;
+  }
 </style>
